c6715a518ee92832cbd4a5713d0036b5ea2eef32
Brent H <118001634+BrentHilton@users.noreply.github.com>
2022-12-23 08:13:50 +0000

    major optimization, added a bunch of new features including a !bird command

--- public.py
+++ public.py
@@ -1,84 +1,82 @@
-# DO NOT MESS WITH CODE UNTILL LINE 7 OR THE BOT WILL NOT START
-
-import requests
 import discord
+import requests
+import random
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-# cat & cat breed example
+#dog && cat links
 cat_urls = {
     "cat": "https://api.thecatapi.com/v1/images/search?limit=1",
     "maine coon": "https://api.thecatapi.com/v1/images/search?limit=1&breed_id=mcoo",
 }
-
-# dog & dog breed example
 dog_urls = {
     "dog": "https://api.thedogapi.com/v1/images/search?limit=1",
     "shiba": "https://api.thedogapi.com/v1/images/search?limit=1&breed_id=222",
 }
 
-# cat api key
+#api keys
 for cat, url in cat_urls.items():
-    cat_urls[cat] = f"{url}&api_key=REPLACE_WITH_API_KEY"
+    cat_urls[cat] = f"{url}&api_key=API_KEY"
 
-# dog api key
 for dog, url in dog_urls.items():
-    dog_urls[dog] = f"{url}&api_key=REPLACE_WITH_API_KEY"
+    dog_urls[dog] = f"{url}&api_key=API_KEY"
 
-# help commands data (what will be in the help command)
+#contents of the !help command
 cathelp ="""
-add anything here.. it will be your help command
-make sure to add newlines (its a multiline string)
+**main animals**
+> !cat [generates a random cat picture]
+> !dog [generates a random dog picture]
+> !bird [generates a random bird picture]
+**different dog breeds!!**
+> !shiba
+**different cat breeds!!**
+> !maine coon
 """
 
-# status
+#status
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="ANYTHING_U_WANT_IT_TO_SAY"))
-    # .playing, .watching .streaming .listening are all valid options
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="how to get treats"))
+    print('Watching status set to "free treats simulator" successfully.')
 
+#listening for the !{breed} commands
 @client.event
 async def on_message(message):
-# dog and cat commands
-    if message.content == "!cat":
-        response = requests.get(cat_urls["cat"])
+    breed_commands = {
+        "!cat": ("cat", cat_urls["cat"]),
+        "!dog": ("dog", dog_urls["dog"]),
+        "!shiba": ("Shiba Inu", dog_urls["shiba"]),
+        "!maine coon": ("Maine Coon", cat_urls["maine coon"]),
+    }
+    #cat && dog commands
+    if message.content in breed_commands:
+        breed_name, breed_url = breed_commands[message.content]
+        response = requests.get(breed_url)
         data = response.json()
-        cat_url = data[0]["url"]
-        embed = discord.Embed()
-        embed.set_image(url=cat_url)
-        await message.channel.send(embed=embed)
+        breed_image_url = data[0]["url"]
+        color = random.randint(0, 16777215)
+        embed = discord.Embed(color=color)
+        embed.set_image(url=breed_image_url)
+        await message.channel.send(f"Here is a {breed_name}:", embed=embed)
 
-    if message.content == "!dog":
-        response = requests.get(dog_urls["dog"])
+    #bird command
+    if message.content == "!bird":
+        color = random.randint(0, 16777215)
+        embed = discord.Embed(color=color)
+        response = requests.get("https://shibe.online/api/birds?count=1&urls=true&httpsUrls=false")
         data = response.json()
-        cat_url = data[0]["url"]
-        embed = discord.Embed()
-        embed.set_image(url=cat_url)
-        await message.channel.send(embed=embed)
-#dog breed
-    if message.content == "!shiba":
-        response = requests.get(dog_urls["shiba"])
-        data = response.json()
-        cat_url = data[0]["url"]
-        embed = discord.Embed()
-        embed.set_image(url=cat_url)
-        await message.channel.send(embed=embed)
+        bird_url = data[0]
+        embed.set_image(url=bird_url)
+        await message.channel.send(f"Here is a bird:", embed=embed)
 
-# cat breed
-    if message.content == "!maine coon":
-        response = requests.get(cat_urls["maine coon"])
-        data = response.json()
-        cat_url = data[0]["url"]
-        embed = discord.Embed()
-        embed.set_image(url=cat_url)
-        await message.channel.send(embed=embed)
-
-# the actual help command itself
-    if message.content.startswith('!help'):
-        embed = discord.Embed(title="!help", description="Here is a list of commands I can do‎ ‎ ‎ ‎ ‎ ‎" + "*farts cutely*")
+    #help command
+    elif message.content.startswith('!help'):
+        color = random.randint(0, 16777215)
+        embed = discord.Embed(title="!help", description="Here is a list of commands I can do...... ***farts cutely***", color=color)
         embed.add_field(name="Commands", value=cathelp)
         await message.channel.send(embed=embed)
 
-# add your bots token here or it will not run obviously..
-client.run('YOUR_BOT_TOKEN')
+
+# add your bot token here
+client.run('TOKEN')
