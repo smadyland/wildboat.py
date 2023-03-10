396fdea0a43fc03fcf4d40780df8e8717b99fee0
smadyland <118001634+smadyland@users.noreply.github.com>
2023-03-09 22:57:55 -0600

    add /raccoon command

--- wildboat.py
+++ wildboat.py
@@ -1,3 +1,4 @@
+import requests
 import discord
 import os
 from discord.ext import commands
@@ -16,5 +17,16 @@ async def on_ready():
 @client.tree.command(name="ping",description="Test bot responsiveness")
 async def ping(Interaction: discord.Interaction):
     await Interaction.response.send_message("Pong!")
+
+@client.tree.command(name="raccoon", description="Get a random raccoon fact and image")
+async def raccoon(Interaction: discord.Interaction):
+    response = requests.get("https://some-random-api.ml/animal/raccoon")
+    if response.status_code == 200:
+        data = response.json()
+        embed = discord.Embed(title="Random Raccoon Fact", description=data["fact"], color=discord.Color.blurple())
+        embed.set_image(url=data["image"])
+        await Interaction.response.send_message(embed=embed)
+    else:
+        await Interaction.response.send_message("Failed to fetch data from the API")
 
 client.run(TOKEN)
